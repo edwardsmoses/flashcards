875cbbdf1189cdadbc75d1558864fa3cee7ada03
Edwards Moses <edwardsmoses3@gmail.com>
2020-12-31 23:27:03 +0100

    test the express web server...
    it works

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express from 'express';
+
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.send("It works! Yup it's working");
+});
+
+app.listen(PORT, () => {
+    console.log(`Node Express Server is listening on PORT ${PORT}`);
+})
