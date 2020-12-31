7c6f2855b180d2bbff61bf1f424fd5b1fe8aa4d5
Edwards Moses <edwardsmoses3@gmail.com>
2020-12-31 23:27:28 +0100

    connecting mongoose to the MongoDb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,13 @@ import express from 'express';
 const app = express();
 const PORT = 4000;
 
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost/flashcards_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+});
+
 app.get('/', (req, res) => {
     res.send("It works! Yup it's working");
 });
